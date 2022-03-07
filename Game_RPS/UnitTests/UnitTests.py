284637d78b4c500124927f5ced0c804cef3c6717
Sheetalr29 <101046069+Sheetalr29@users.noreply.github.com>
2022-03-07 15:55:48 +0100

    Add files via upload
    Added comments

--- Game_RPS/UnitTests/UnitTests.py
+++ Game_RPS/UnitTests/UnitTests.py
@@ -1,6 +1,7 @@
 import unittest
 from Game_RPS import game
 
+#Class to perform the unit tests for Rock Paper Scissor game
 class UnitTests(unittest.TestCase):
      def test_articleChoice(self):
          pass
